9ff614bafe47168b5e6ebb8181be8873055ddd3d
jinsolJEONG <config8@naver.com>
2023-01-18 17:59:38 +0900

    230118 수업 끝

---
+++ 2. web/230118/2.split.js
@@ -0,0 +1,8 @@
+const a  = "치킨|삼겹살|마라탕";
+const arr = a.split("|");
+console.log(arr);
+
+for(let y=0; y < arr.length; y++){
+  console.log(arr[y]);
+}
+
---
+++ 민코딩/230118/230118/230118.cpp
@@ -0,0 +1,89 @@
+﻿//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    char arr[3][3] = {  };
+//    char a = 'A';
+//    for (int y = 0; y < 3; y++)
+//    {
+//        for (int x = 0; x < 3; x++)
+//        {
+//            arr[y][x] = a; 
+//            a++;
+//        }
+//    }
+//    int y1, x1, y2, x2;
+//    cin >> y1 >> x1 >> y2 >> x2;
+//    char temp;
+//    temp = arr[y1][x1];
+//    arr[y1][x1] = arr[y2][x2];
+//    arr[y2][x2] = temp;
+//    for (int y = 0; y < 3; y++)
+//    {
+//        for (int x = 0; x < 3; x++)
+//        {
+//            cout << arr[y][x];
+//        }
+//        cout << endl;
+//    }
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    char arr[6][2] = {  };
+//    for (int y = 0; y < 6; y++)
+//    {
+//        for (int x = 0; x < 2; x++)
+//        {
+//            cin >> arr[y][x]; 
+//        }
+//    }
+//    int cnt = 0;
+//    for (int y = 0; y < 6; y++)
+//    {
+//        char temp;
+//        if (arr[y][0] < arr[y][1])
+//        {
+//            temp = arr[y][0];
+//            arr[y][0] = arr[y][1];
+//            arr[y][1] = temp;
+//            cnt++;
+//        } 
+//    }
+//
+//    for (int y = 0; y < 6; y++)
+//    {
+//        for (int x = 0; x < 2; x++)
+//        {
+//            cout << arr[y][x] << ' ';
+//        }
+//        cout << endl;
+//    }
+//    cout << cnt << "명";
+//    return 0;
+//}
+
+#include <iostream>
+using namespace std;
+
+void BBQ(int a, int b) {
+    cout << "합:" << a + b << endl;
+    cout << "차:" << a - b << endl;
+    cout << "곱:" << a * b << endl;
+    cout << "몫:" << a / b << endl;
+
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    BBQ(a, b);
+    return 0;
+}
+
