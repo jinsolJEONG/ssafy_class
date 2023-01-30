cfb01a4b513aa0e82aed93f0e623db1984ea5067
jinsolJEONG <config8@naver.com>
2023-01-30 17:40:14 +0900

    230130 수업 끝

---
+++ 3. algorithm/230130-offline/230130-offline/230130-offline.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include<vector>
+using namespace std;
+
+vector<int> v; 
+int main()
+{	
+	v.push_back(5);
+	v.push_back(3);
+
+	v.pop_back();
+	return 0;
+}
+
---
+++ 민코딩/230130/230130/230130.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	string str;
+	cin >> str;
+	string k;
+	int a, b, c;
+	cin >> a >> b >> c;
+	k = str.substr(a, b-a+1);
+	for (int i = 0; i < c; i++)
+	{
+		cout << k;
+	}
+	return 0;
+}
