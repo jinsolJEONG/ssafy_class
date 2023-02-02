5cce19880957b996ca316791e40a399524d88844
jinsolJEONG <config8@naver.com>
2023-02-02 17:58:55 +0900

    230202 수업 끝

---
+++ 3. algorithm/230201/230201/230201.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+string str;
+int dat[256] = {};
+
+int main() {
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		dat[str[i]]++;
+	}
+
+	cout << (int)'_';
+	return 0;
+}
--- 민코딩/230131/230131/230131.cpp
+++ 민코딩/230131/230131/230131.cpp
@@ -1,35 +1,10 @@
 ﻿#include<iostream>
-#include<vector>
+#include<cstring>
 using namespace std;
-int a, b;
-int map[10][10] = {
-	0,0,0,0,0,0,0,
-	0,0,0,1,0,1,1,
-	0,1,0,0,1,0,0,
-	0,0,0,0,1,0,0,
-	0,0,0,0,0,1,0,
-	0,1,0,0,0,0,0,
-	0,0,0,0,0,0,0,
-};
-int cnt = 0;
-void dfs(int now) {
-	
-	for (int i = a; i < b; i++)
-	{
-		if (map[now][i] == 0)continue;
-		cnt++;
-		dfs(i);
-		cnt++;
-	}
-	
-}
+
+
 
 int main() {
-	cin >> a >> b;
-	dfs(1);
-
-		cout << cnt;
-
 
 	return 0;
 }
