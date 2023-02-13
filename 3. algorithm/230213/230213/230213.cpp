1afd5950e2497a6588fa2092da39d32aa2b0b4d6
jinsolJEONG <config8@naver.com>
2023-02-13 17:59:33 +0900

    230213 수업 끝

---
+++ 3. algorithm/230213/230213/230213.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+string strNum;
+
+string reverse(string const& str)
+{
+	string rev(str.rbegin(), str.rend());
+	return rev;
+}
+
+int main()
+{
+	int num, n;
+	cin >> num >> n;
+	for (int i = 0; i < n; i++)
+	{
+		num = num * 2;
+		string str = to_string(num);
+		str = reverse(str);
+		num = stoi(str);
+	}
+
+	cout << num;
+
+	return 0;
+}
